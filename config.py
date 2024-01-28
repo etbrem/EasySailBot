69fa9346576fe64fb85f2b2af0c235d77a054ab1
etbrem <etbrem2@gmail.com>
2024-01-29 00:31:33 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+DIR_TV_SHOWS = '/plex/media/TV Shows'
+DIR_MOVIES = '/plex/media/Movies'
+
+DIR_TV_SHOWS = r'C:\Users\USER\Videos\TV Shows'
+DIR_MOVIES = r'C:\Users\USER\Videos\Movies'
+
+
+API_TOKEN = '123456789:AABBCCDDEEFFGGjgfidogfdfdsdsfsdfsdfsd'
+AUTHENTICATED_USER_IDS = [1, 232121321, 123213123]
