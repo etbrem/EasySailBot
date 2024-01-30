b3159997354d42a8eada56cf8956d5fd51c08d53
etbrem <etbrem2@gmail.com>
2024-01-30 23:25:19 +0200

    Update transmission_ctl.py

--- transmission_ctl.py
+++ transmission_ctl.py
@@ -3,6 +3,9 @@ import math
 import transmissionrpc
 
 
+######################################################################
+# Misc
+######################################################################
 
 def convert_size(size_bytes, tag=True):
    if size_bytes == 0:
@@ -16,12 +19,11 @@ def convert_size(size_bytes, tag=True):
    else:
         return s
 
+######################################################################
+# RPC
+######################################################################
+
 TRANSMISSION_RPC_OBJECT = None
-
-
-
-DIR_TV_SHOWS = '/plex/media/TV Shows'
-DIR_MOVIES = '/plex/media/Movies'
 
 
 def create_transmission_rpc():
@@ -36,7 +38,6 @@ def get_transmission_rpc():
 
     return TRANSMISSION_RPC_OBJECT
 
-
 def add_torrent_to_dir(magnet, download_dir):
     print("add_torrent_to_dir", download_dir, magnet)
 
@@ -45,71 +46,107 @@ def add_torrent_to_dir(magnet, download_dir):
     torrent = tc.add_torrent(magnet, download_dir=download_dir)
     return torrent_repr(torrent)
 
-
-def add_tv_show(magnet):
-    return add_torrent_to_dir(magnet, DIR_TV_SHOWS)
-
-
-def add_movie(magnet):
-    return add_torrent_to_dir(magnet, DIR_MOVIES)
-
-
-def iter_torrents(translation=lambda t: t):
+def iter_torrents(transformation=lambda t: t):
     for torrent in get_transmission_rpc().get_torrents():
-        yield translation(torrent)
+        yield transformation(torrent)
+
+
+######################################################################
+# Torrent
+######################################################################
 
 def get_torrent_size(torrent):
+    torrent = make_torrent(torrent)
     return sum(f['size'] for f in torrent.files().values())
 
+def get_torrent_completed(torrent):
+    torrent = make_torrent(torrent)
+    return sum(f['completed'] for f in torrent.files().values())
 
 def torrent_repr(torrent):
+    if not torrent:
+        return ''
+
     return '{id}: {name}'.format(id=torrent.id, name=torrent.name)
     
 def torrent_status_repr(torrent):
+    torrent = make_torrent(torrent)
+
     torrent_id = torrent.id
     torrent_name = torrent.name
-            
-    try:
-        torrent_percent = str(int(torrent.progress)) + "%"
-    except:
-        torrent_percent = "?? %"
-    
-    try:
-        torrent_status = torrent.status
-    except:
-        torrent_status = "Unknown status"
-    
-    try:
-        size = get_torrent_size(torrent)
-        done = float(convert_size(size, tag=False)) * (torrent.progress/100.0)
-        
-        torrent_size = f"{done} / {convert_size(size)}"
-    except:
-        torrent_size = convert_size(get_torrent_size(torrent))
-    
-    return '{torrent_id}: {torrent_status} {torrent_percent} {torrent_size}\n{torrent_name}'.format(**locals())
+    torrent_status = torrent.status
+
+    torrent_completed = get_torrent_completed(torrent)
+    torrent_size = get_torrent_size(torrent)
+
+    torrent_completed_str = convert_size(torrent_completed)
+    torrent_size_str = convert_size(torrent_size)
+
+    torrent_percent = int(100.0 * torrent_completed / torrent_size )
+
+    return '{torrent_id}: {torrent_status} {torrent_percent}% {torrent_completed_str}/{torrent_size_str}\n{torrent_name}'.format(**locals())
 
 def get_torrent(torrent_id):
     tc = get_transmission_rpc()
 
     return tc.get_torrent(int(torrent_id))
 
+def make_torrent(torrent_or_id):
+    if isinstance(torrent_or_id, (int, str)):
+        return get_torrent(torrent_or_id)
+    return torrent_or_id
 
-def start_torrent(torrent_id):
-    torrent = get_torrent(torrent_id)
+def start_torrent(torrent):
+    torrent = make_torrent(torrent)
     return torrent.start()
 
-
-def stop_torrent(torrent_id):
-    torrent = get_torrent(torrent_id)
+def stop_torrent(torrent):
+    torrent = make_torrent(torrent)
     return torrent.stop()
 
-
-
-def delete_torrent(torrent_id):
-    torrent_id = int(torrent_id)
-
-    torrent = get_torrent(torrent_id)
+def delete_torrent(torrent):
+    torrent = make_torrent(torrent)
+    torrent_id = torrent.id
+    del torrent
 
     tc = get_transmission_rpc()
     return tc.remove_torrent(torrent_id, delete_data=True)
+
+
+######################################################################
+# Torrent File
+######################################################################
+
+class TorrentFile(object):
+    PROPERTY_NAMES = ['selected', 'priority', 'size', 'name', 'completed']
+
+    def __init__(self, torrent_id, file_id, properties):
+        self.torrent_id = torrent_id
+        self.file_id = file_id
+        self._properties = properties
+        
+        for property_name in self.PROPERTY_NAMES:
+            setattr(self, property_name, properties.get(property_name))
+
+        self.percent = int(100 * float(self.completed) / self.size)
+
+    def __str__(self):
+        return f'{self.torrent_id}.{self.file_id}: {self.name}'
+
+    def __repr__(self):
+        selected_status = ' DISABLED' if not self.selected else ''
+        return str(self) + f' {self.percent}% {convert_size(self.size)}{selected_status}'
+
+def iter_torrent_files(torrent):
+    torrent = make_torrent(torrent)
+
+    for file_id, properties in torrent.files().items():
+        yield TorrentFile(torrent.id, file_id, properties)
+
+
+def toggle_torrent_file(torrent_file):
+    tc = get_transmission_rpc()
+
+    return tc.set_files({ 
+        torrent_file.torrent_id: { torrent_file.file_id: {'selected': not torrent_file.selected} }
+    })
