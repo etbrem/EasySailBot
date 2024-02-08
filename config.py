296dd617dcce212666866b5dde88bfe95fa9f8f2
etbrem <etbrem2@gmail.com>
2024-02-09 01:21:05 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,7 @@
-DIR_TV_SHOWS = '/plex/media/TV Shows'
-DIR_MOVIES = '/plex/media/Movies'
-
-DIR_TV_SHOWS = r'C:\Users\USER\Videos\TV Shows'
-DIR_MOVIES = r'C:\Users\USER\Videos\Movies'
-
+# DIR_TV_SHOWS = '/plex/media/TV Shows'
+# DIR_MOVIES = '/plex/media/Movies'
+DIR_TV_SHOWS = r"C:\Users\USER\Videos\TV Shows"
+DIR_MOVIES = r"C:\Users\USER\Videos\Movies"
 
 API_TOKEN = '110201543:AAHdqTcvCH1vGWJxfSeofSAs0K5PALDsaw'
 AUTHENTICATED_USER_IDS = [1, 232121321, 123213123]
-
-TRANSMISSION_IP = '127.0.0.1
